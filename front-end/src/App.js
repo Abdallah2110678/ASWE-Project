b22af51af22c4f05e2e90f6538e6e505a99bc08f
Moniem <abdulrahman2111656@miuegypt.edu.eg>
2024-03-07 22:44:46 +0200

    refactor route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from "./components/common/header/Header"
-import { BrowserRouter as Router ,Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router ,Routes, Route, Outlet } from "react-router-dom"
 import About from "./components/about/About"
 import CourseHome from "./components/allcourses/CourseHome"
 import Team from "./components/team/Team"
@@ -9,23 +9,38 @@ import Blog from "./components/blog/Blog"
 import Contact from "./components/contact/Contact"
 import Footer from "./components/common/footer/Footer"
 import Home from "./components/home/Home"
-
+import Aside_Admin from './components/common/asidebar-admin/asidebar';
+import Dashboard from "./components/admin-dashboaerd/Dashboard";
 function App() {
 
   return (
     <>
     <Router>
-      <Header />
       <Routes>
-      <Route exact path='/' element={<Home />} />
-      <Route exact path='/about' element={<About />} />
-      <Route exact path='/courses' element={<CourseHome />} />
-      <Route exact path='/team' element={<Team />} />
-      <Route exact path='/pricing' element={<Pricing />} />
-      <Route exact path='/journal' element={<Blog />} />
-      <Route exact path='/contact' element={<Contact />} />
-    </Routes>
-      <Footer />
+        <Route path="/admin" element={<><Aside_Admin/>
+          <Outlet />
+        </>}>
+          <Route path="dashboard" element={ < Dashboard /> } />
+        </Route>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Outlet />
+              <Footer />
+            </>
+          }
+        >
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<CourseHome />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/journal" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </Router>
   </>
   );
