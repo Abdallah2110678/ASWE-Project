4a3ca766eb975da5c0331c14d4ff5578ccc99234
EsraaHassann <147067443+EsraaHassann@users.noreply.github.com>
2024-03-10 23:21:15 +0200

    models

---
+++ demo/src/main/java/com/example/aswe/demo/models/Student.java
@@ -0,0 +1,157 @@
+package com.example.aswe.demo.models;
+
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+
+@Entity
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+    private String Fname;
+    private String Lname;
+    private String password;
+    private String gender;
+    private LocalDate dob;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(Long id, String Fname, String Lname, String password, String gender, LocalDate dob, String email) {
+        this.id = id;
+        this.Fname = Fname;
+        this.Lname = Lname;
+        this.password = password;
+        this.gender = gender;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return this.Fname;
+    }
+
+    public void setFname(String Fname) {
+        this.Fname = Fname;
+    }
+
+    public String getLname() {
+        return this.Lname;
+    }
+
+    public void setLname(String Lname) {
+        this.Lname = Lname;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDob() {
+        return this.dob;
+    }
+
+    public void setDob(LocalDate dob2) {
+        this.dob = dob2;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Student id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public Student Fname(String Fname) {
+        setFname(Fname);
+        return this;
+    }
+
+    public Student Lname(String Lname) {
+        setLname(Lname);
+        return this;
+    }
+
+    public Student password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    public Student gender(String gender) {
+        setGender(gender);
+        return this;
+    }
+
+    public Student dob(LocalDate dob) {
+        setDob(dob);
+        return this;
+    }
+
+    public Student email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Student)) {
+            return false;
+        }
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(Fname, student.Fname) && Objects.equals(Lname, student.Lname) && Objects.equals(password, student.password) && Objects.equals(gender, student.gender) && Objects.equals(dob, student.dob) && Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, Fname, Lname, password, gender, dob, email);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", Fname='" + getFname() + "'" +
+            ", Lname='" + getLname() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", gender='" + getGender() + "'" +
+            ", dob='" + getDob() + "'" +
+            ", email='" + getEmail() + "'" +
+            "}";
+    }
+
+
+}
