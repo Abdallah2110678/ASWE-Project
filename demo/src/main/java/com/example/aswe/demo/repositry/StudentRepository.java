39e2179a047922ce128b72ec75a06dfad6510c83
EsraaHassann <147067443+EsraaHassann@users.noreply.github.com>
2024-03-11 15:11:10 +0200

    student repo

--- demo/src/main/java/com/example/aswe/demo/repositry/StudentRepository.java
+++ demo/src/main/java/com/example/aswe/demo/repositry/StudentRepository.java
@@ -1,8 +1,9 @@
-package com.example.aswe.demo.repositories;
+package com.example.aswe.demo.repositry;
 
 import com.example.aswe.demo.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository <Student,Long>{
     Student findByEmail(String email);
+
 }
