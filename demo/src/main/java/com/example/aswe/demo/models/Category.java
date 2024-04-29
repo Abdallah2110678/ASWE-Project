566e9d71c21c50f4dd372f36f5a5b1a0c8db446d
Moniem <abdulrahman2111656@miuegypt.edu.eg>
2024-04-29 18:00:06 +0300

    create class category

---
+++ demo/src/main/java/com/example/aswe/demo/models/Category.java
@@ -0,0 +1,76 @@
+package  com.example.aswe.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public Category name(String name) {
+        setName(name);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Category)) {
+            return false;
+        }
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+    
+
+}
--- demo/src/main/java/com/example/aswe/demo/models/Course.java
+++ demo/src/main/java/com/example/aswe/demo/models/Course.java
@@ -1,7 +1,6 @@
 package com.example.aswe.demo.models;
 
 public class Course {
-    private int id;
-    private String title;
+    
     
 }
