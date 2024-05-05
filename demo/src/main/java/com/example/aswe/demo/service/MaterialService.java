c9ee78c49a22e010fd3ee7422bf2baa4ec3a4c9e
Moniem <abdulrahman2111656@miuegypt.edu.eg>
2024-05-06 01:17:45 +0300

    create  MaterialService

---
+++ demo/src/main/java/com/example/aswe/demo/service/MaterialService.java
@@ -0,0 +1,19 @@
+package com.example.aswe.demo.service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.aswe.demo.models.CourseMaterial;
+
+
+
+public interface MaterialService {
+
+    
+    CourseMaterial uploadCourseMaterial(String path, MultipartFile file) throws IOException;
+    InputStream getCourseMaterial(String path, String fileName,Long id) throws FileNotFoundException;
+
+}
---
+++ demo/src/main/java/com/example/aswe/demo/service/MaterialServiceImp.java
@@ -0,0 +1,59 @@
+package com.example.aswe.demo.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.aswe.demo.models.CourseMaterial;
+import com.example.aswe.demo.repository.CourseMaterialRepository;
+
+
+@Service
+public class MaterialServiceImp implements MaterialService {
+
+
+    @Autowired
+    private CourseMaterialRepository courseMaterialRepository;
+    @Override
+    public CourseMaterial uploadCourseMaterial(String path, MultipartFile file) throws IOException {
+
+        CourseMaterial courseMaterial = new CourseMaterial();
+        String fileName = file.getOriginalFilename();
+
+        // trying to generate random name and uniques
+        String randomId = UUID.randomUUID().toString();
+        String finalName = randomId.concat(fileName.substring(fileName.lastIndexOf(".")));
+
+        // string full path
+        String filePath = path + File.separator + finalName;
+
+        // creating directory to save file
+        File f = new File(path);
+        if (!f.exists()) {
+            f.mkdir();
+        }
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+        courseMaterial.setVideoFileName(finalName);
+
+        return courseMaterial;
+
+    }
+
+    @Override
+    public InputStream getCourseMaterial(String path, String fileName, Long id) throws FileNotFoundException {
+       String fullPath = path + File.separator + fileName;
+        InputStream inputStream = new FileInputStream(fullPath);
+
+        return inputStream;
+    }
+
+}
