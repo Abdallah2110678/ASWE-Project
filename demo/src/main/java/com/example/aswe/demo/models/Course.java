d15181ecd5aa64b0655df20bf846608656fac893
Moniem <abdulrahman2111656@miuegypt.edu.eg>
2024-05-07 18:58:58 +0300

    fixed an error

--- demo/src/main/java/com/example/aswe/demo/models/Course.java
+++ demo/src/main/java/com/example/aswe/demo/models/Course.java
@@ -3,3 +3,55 @@
 
 
 
+package com.example.aswe.demo.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Course {
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private String tags;
+    private Double price;
+    private Date date;
+    
+    
+    @ManyToOne
+    private User user;
+    
+    @ManyToOne
+    private Category category;
+
+
+    @OneToMany(mappedBy = "course")
+    @JsonManagedReference
+    private List<CourseMaterial> courseMaterials;
+}
+
+
